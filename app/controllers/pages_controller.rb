8d22532c09c5cc503f7850d7843554818ca4138f
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-08-31 21:23:30 -0500

    form added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,12 @@ class PagesController < ApplicationController
   def all
     @all = Contact.all
   end
+
+  def add_contact
+  end
+
+  def create_contact
+    @contact = Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone])
+    @thanks = 'your information has been saved'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/one' => 'pages#one'
   get '/all' => 'pages#all'
+  get '/add_contact' => 'pages#add_contact'
+  post '/add_contact' => 'pages#create_contact'
 end
